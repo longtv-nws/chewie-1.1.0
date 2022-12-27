232d58636f57ae8fa12a2b20241e8d4d8aa14ebf
longtv <longtv@newwave.vn>
2022-12-27 17:04:11 +0700

    Initial commit

---
+++ lib/chewie.dart
@@ -0,0 +1,7 @@
+library chewie;
+
+export 'src/chewie_player.dart';
+export 'src/chewie_progress_colors.dart';
+export 'src/cupertino_controls.dart';
+export 'src/material_controls.dart';
+export 'src/subtitle_model.dart';
